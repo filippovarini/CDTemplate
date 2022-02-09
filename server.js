7168af9a2802a4dae839f07e1233c7c20e43d807
fv220 <fv220@ic.ac.uk>
2022-02-09 23:48:29 +0000

    app setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.get("*", (_, res) => {
+  res.send("Hello world!")
+})
+
+const PORT = 5001
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}..`))
