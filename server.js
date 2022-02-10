a0554b6bb2bd07affcf67269949946ddcbd88845
fv220 <fv220@ic.ac.uk>
2022-02-10 01:42:21 +0000

    server.js fix

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("*", (_, res) => {
   res.send("Hello world")
 })
 
-const PORT = process.ENV.PORT || 5001
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}..`))
